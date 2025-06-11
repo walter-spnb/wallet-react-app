aab3bf7a5e6a744c4540ab17f338433320022f53
bong.channarith <bong.channarith@sathapana.com.kh>
2025-06-11 17:40:48 +0700

    TMP

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer' // Import directly
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer() // Use the imported version
+      ]
+    }
+  }
 })
